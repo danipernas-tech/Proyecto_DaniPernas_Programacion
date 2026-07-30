2f08f66b94682ef417051ea8060c968877d4213f
danipernas-tech <danielpernasbenito@gmail.com>
2026-07-30 09:27:02 +0200

    Clase camarero con sus atributos(encapsulación), getter, setter y override

---
+++ src/Camarero.java
@@ -0,0 +1,35 @@
+public class Camarero {
+
+    //Atributos privados de Camarero
+    private String nombre;
+    private String codigoEmpleado;
+
+    //Constructor -> guarda nombre y código de empleado
+    public Camarero(String nombre, String codigoEmpleado) {
+        this.nombre = nombre;
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Código de empleado
+    public String getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(String codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " - Código: " + codigoEmpleado;
+    }
+}
