4c57e8a4c3474b7bd1303635eff773e99daa86b6
danipernas-tech <danielpernasbenito@gmail.com>
2026-08-23 12:41:51 +0200

    Aplicar descuentos antes de enseñar los dos tickets

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     static void main() {
 
+        //Ticket 1
         //Creamos el cliente
         Cliente cliente = new Cliente("Ana López", "600123456", 25);
 
@@ -21,17 +22,40 @@ public class Main {
         ticket.agregarProducto(bocadillo);
         ticket.agregarProducto(zumo);
 
-        //Enseñamos el ticket completo + total
+        //Aplicamos un descuento usando la interfaz Descontable
+        double porcentaje = 10;
+        Descontable productoDescontable = cafe;
+        cafe.setPrecio(productoDescontable.aplicarDescuento(porcentaje));
+
+        //Enseñamos el ticket completo + descuento
         ticket.mostrarTicket();
 
-        //Aplicamos un descuento usando la interfaz Descontable
         System.out.println();
-        Descontable productoDescontable = cafe;   //El café es descontable
-        double porcentaje = 10;
-        double precioConDescuento = productoDescontable.aplicarDescuento(porcentaje);
-
         System.out.println("Descuento aplicado al café: " + (int) porcentaje + "%");
-        System.out.println("Precio final del café: " + String.format("%.2f", precioConDescuento) + " €");
+        System.out.println("Precio final del café: " + String.format("%.2f", cafe.getPrecio()) + " €");
+
+
+        //Ticket 2
+        System.out.println();
+        System.out.println();
+
+        Cliente cliente2 = new Cliente("Luis", "600885323", 55);
+        Camarero camarero2 = new Camarero ("Silvia", "C02");
+
+        Bebida batido = new Bebida("Batido Oreo", 4.50, "grande");
+        Comida desayuno = new Comida("Tostada de jamon con tomate", 3.90, true);
+
+        Ticket ticket2 = new Ticket(cliente2, camarero2);
+        ticket2.agregarProducto(batido);
+        ticket2.agregarProducto(desayuno);
+
+        batido.setPrecio(batido.aplicarDescuento(15));
+
+        ticket2.mostrarTicket();
+
+        System.out.println();
+        System.out.println("Descuento aplicado al batido: 15%");
+        System.out.println("Precio final batido: " + String.format("%.2f" , batido.getPrecio()) + " €");
     }
 
 
